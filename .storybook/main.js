2d135922d40cb1d645de39901747da6d76718a99
Kai Vandivier <kpvandivier@gmail.com>
2021-01-17 14:26:21 +0100

    Make Intro story first in list

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
   "stories": [
+    // Get the Intro story to load first in order
+    "../src/components/Intro.stories.mdx",
+    // Automatically import all appropriate stories
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
